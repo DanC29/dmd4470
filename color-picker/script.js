ed76899b9ba72151528249e9ead96be976dee84a
Dan Collette <daniel.collette@uconn.edu>
2022-02-02 19:03:14 -0500

    color picker final
    final commit

--- color-picker/script.js
+++ color-picker/script.js
@@ -12,7 +12,36 @@ function changeColor() {
   );
 }
 // Presets
-function presetTemplates() {
-  var modeSwitch = document.getElementById("main-bg");
+/* function presetTemplates() {
+  var modeSwitch = document.querySelector("body");
   modeSwitch.classList.toggle("preset-mode-1");
+} */
+
+/* var bod = document.querySelector("body");
+bod.classList.add("dark", "light");
+
+console.log(bod.classList.contains("dark"));
+
+let mode = [bod.classList];
+console.log(mode);
+
+function presetTemplates(event) {
+  if ((mode = 0)) {
+    bod.classList.toggle("dark");
+  } else if ((mode = 1)) {
+    bod.classList.toggle("light");
+  }
+} */
+var bod = document.querySelector("body");
+
+colorArray = ["orange", "purple"];
+
+function presetTemplates() {
+  mode = colorArray.shift(bod.classList);
+  colorArray.push(mode);
+
+  var modeSwitch = document.querySelector("body");
+  modeSwitch.classList.toggle("preset-mode-1");
+
+  bod.style.backgroundColor = mode;
 }
