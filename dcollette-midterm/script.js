e33c3070f1f0f64717bdb33c9180c215170d6be7
Dan Collette <daniel.collette@uconn.edu>
2022-02-20 23:30:08 -0500

    midterm-second
    Kinda got delete and checked buttons working

--- dcollette-midterm/script.js
+++ dcollette-midterm/script.js
@@ -2,8 +2,84 @@ const item_input = document.querySelector("#item_input");
 const add_item_btn = document.querySelector("#add_new_task");
 const my_list = document.querySelector("#my_list");
 
+var cardOBJ = document.querySelector(".card");
+
 add_item_btn.addEventListener("click", addToDB);
 
+db.collection("list_items").onSnapshot((querySnapshot) => {
+  my_list.innerHTML = "";
+
+  querySnapshot.forEach((doc) => {
+    console.log("DOC: ", doc);
+
+    //CARD
+    var cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "col-md-8");
+    cardDiv.id = doc.data().id;
+    //TITLE
+    var cardTitle = document.createElement("h4");
+    cardTitle.innerHTML = doc.data().name;
+    cardTitle.classList.add("card-title");
+    cardDiv.appendChild(cardTitle);
+    //BTN DIV
+    var btnsDiv = document.createElement("div");
+    btnsDiv.classList.add("btnDiv");
+    cardDiv.appendChild(btnsDiv);
+    //CHECK BTN
+    var cardCheck = document.createElement("button");
+    cardCheck.innerText = "Check Item";
+    cardCheck.classList.add("btn", "card-complete");
+    cardCheck.addEventListener("click", function () {
+      cardComplete(doc.id);
+    });
+    btnsDiv.appendChild(cardCheck);
+    //DELETE BTN
+    var cardDelete = document.createElement("button");
+    cardDelete.innerText = "Delete Item";
+    cardDelete.classList.add("btn", "cardDelete");
+    cardDelete.addEventListener("click", function () {
+      removeCard(doc.id);
+    });
+    btnsDiv.appendChild(cardDelete);
+
+    my_list.appendChild(cardDiv);
+  });
+});
+
+//CHECK FUNCTION
+function cardComplete(id) {
+  var cardRef = db.collection("list_items").doc(id);
+
+  const cardBgRef = document.querySelector(".card");
+
+  // Set the "capital" field of the city 'DC'
+  return cardRef
+    .update({
+      status: "complete",
+    })
+    .then(() => {
+      console.log("Document successfully updated!");
+      cardBgRef.style.backgroundColor = "green";
+    })
+    .catch((error) => {
+      // The document probably doesn't exist.
+      console.error("Error updating document: ", error);
+    });
+}
+
+//DELETE FUNCTION
+function removeCard(id) {
+  db.collection("list_items")
+    .doc(id)
+    .delete()
+    .then(() => {
+      console.log("Document successfully deleted!");
+    })
+    .catch((error) => {
+      console.error("Error removing document: ", error);
+    });
+}
+//ADD DOC TO COLLECTION
 function addToDB() {
   var itemName = item_input.value;
   // Add a new document with a generated id.
@@ -19,24 +95,6 @@ function addToDB() {
       console.error("Error adding document: ", error);
     });
 }
-
-db.collection("list_items").onSnapshot((querySnapshot) => {
-  my_list.innerHTML = "";
-
-  querySnapshot.forEach((doc) => {
-    console.log("DOC: ", doc);
-    var cardDiv = document.createElement("div");
-    cardDiv.classList.add("card", "col-md-8");
-    cardDiv.id = doc.data().id;
-
-    var cardTitle = document.createElement("h4");
-    cardTitle.innerHTML = doc.data().name;
-    cardTitle.classList.add("card-title");
-    cardDiv.appendChild(cardTitle);
-
-    my_list.appendChild(cardDiv);
-  });
-});
 
 /* class Items {
   constructor(name, status) {
